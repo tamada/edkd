2bf9c6bdd01a8485580de98ac4090a60398ed0a9
YKatsuy <84626533+YKatsuy@users.noreply.github.com>
2021-06-01 13:16:35 +0900

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -1,7 +1,7 @@
 package main
 
 func Example_help() {
-	goMain([]string{"/some/path/of/scv", "-h"})
+	goMain([]string{"/some/path/of/edkd", "-h"})
 	// Output:
 	// scv [OPTIONS] <VECTORS...>
 	// OPTIONS
