21787e6b936fc8d9e45776db0787d10525d7b574
YKatsuy <84626533+YKatsuy@users.noreply.github.com>
2021-06-07 17:31:37 +0900

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func helpMessage(originalProgramName string) string {
-	programName := filepath.Base(originalProgramName)
+	name := filepath.Base(originalProgramName)
 	return fmt.Sprintf(`%s [OPTIONS...] [NUMBERs...|CSV]  
 　OPTIONS
 　　-H,--Hubeny           ヒュベニの公式で距離を算出．デフォルトではharversine公式を使用して算出する．
@@ -17,7 +17,7 @@ func helpMessage(originalProgramName string) string {
 　　-h,--help             Usageを表示する．
 　ARGUMENTS
 　　NUMBERs...            二地点の緯度経度を引数にする．デフォルトではポイント1経度，ポイント1緯度，ポイント2経度，ポイント2緯度．但しオプション-xの場合，ポイント1緯度，ポイント2緯度，オプション-yの場合，ポイント1経度，ポイント2経度
-　　CSV                   二地点の緯度経度がまとめられたcsvファイルを引数にする．csvファイルはosmで扱うため．デフォルトでは出力ファイルは"ykgeo_output.csv"，変えたい場合は2つ目の引数で設定する．`, programName)
+　　CSV                   二地点の緯度経度がまとめられたcsvファイルを引数にする．csvファイルはosmで扱うため．デフォルトでは出力ファイルは"ykgeo_output.csv"，変えたい場合は2つ目の引数で設定する．`, name)
 }
 
 func goMain(args []string) int {
