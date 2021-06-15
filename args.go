c32da466d9e91a0713ec4d28720d1e7814ee2ec5
YKatsuy <dogoru@yamauchi.local>
2021-06-15 10:37:21 +0900

    develop

--- args.go
+++ args.go
@@ -8,13 +8,7 @@ import (
 )
 
 type options struct {
-	/*path     string
-	number   bool
-	nonblank bool
-	squeeze  bool
-	table    bool*/
 	help   bool
-	args   []string
 	Hubeny bool
 	samex  bool
 	samey  bool
@@ -30,11 +24,6 @@ func parseArgs(args []string) (*options, error) {
 	opts := &options{}
 	flags := flag.NewFlagSet("edkd", flag.ContinueOnError)
 	flags.Usage = func() { fmt.Println(helpMessage(args[0])) }
-	/*flags.StringVarP(&opts.path, "path", "p", "", "ファイルパス．")
-	flags.BoolVarP(&opts.number, "number", "n", false, "行番号を表示する．")
-	flags.BoolVarP(&opts.nonblank, "number-nonblank", "b", false, "行番号を表示する．ただし空白行には付けない．")
-	flags.BoolVarP(&opts.squeeze, "squeeze", "s", false, "連続した空行を1行にする．")
-	flags.BoolVarP(&opts.table, "table", "t", false, "2つ目以降のファイルでは先頭行を無視する．")*/
 	flags.BoolVarP(&opts.Hubeny, "Hubeny", "H", false, "ヒュベニの公式を使用します．")
 	flags.BoolVarP(&opts.samex, "samex", "x", false, "緯度が同じ場合です．")
 	flags.BoolVarP(&opts.samey, "samey", "y", false, "経度が同じ場合です．")
